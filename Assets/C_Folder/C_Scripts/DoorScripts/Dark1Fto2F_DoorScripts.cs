0797f01149ccfcc3b033c3df81d180491e9d8aae
AejiCheon <aeji4123@naver.com>
2025-03-04 20:24:03 +0900

    darkmapedit 25.03.04

--- Assets/C_Folder/C_Scripts/DoorScripts/Dark1Fto2F_DoorScripts.cs
+++ Assets/C_Folder/C_Scripts/DoorScripts/Dark1Fto2F_DoorScripts.cs
@@ -5,19 +5,47 @@ public class Dark1Fto2F_DoorScripts : MonoBehaviour
 {
     public GameObject Dark1Floor;
     public GameObject Dark2Floor;
+    private Collider2D doorCollider;
+    private bool isOnFirstFloor = true; // 현재 층 상태 (true: 1층, false: 2층)
 
     void Start()
     {
-        Dark2Floor.SetActive(false); // 기본 상태에서 비활성화
+        Dark2Floor.SetActive(false);
+        doorCollider = GetComponent<Collider2D>();
     }
 
     private void OnTriggerEnter2D(Collider2D coll)
     {
-        if (coll.CompareTag("Player")) // 플레이어가 문에 닿았을 때
+        if (coll.CompareTag("Player"))
         {
             Debug.Log("문에 닿음!");
-            Dark2Floor.SetActive(true);
+            ToggleFloors(); // 층 전환
+            StartCoroutine(DisableColliderTemporarily()); // 콜라이더 2초 비활성화
+        }
+    }
+
+    void ToggleFloors()
+    {
+        if (isOnFirstFloor)
+        {
             Dark1Floor.SetActive(false);
+            Dark2Floor.SetActive(true);
+        }
+        else
+        {
+            Dark1Floor.SetActive(true);
+            Dark2Floor.SetActive(false);
+        }
+        isOnFirstFloor = !isOnFirstFloor; // 상태 변경
+    }
+
+    IEnumerator DisableColliderTemporarily()
+    {
+        if (doorCollider != null)
+        {
+            doorCollider.enabled = false;
+            yield return new WaitForSeconds(2f);
+            doorCollider.enabled = true;
         }
     }
 }
